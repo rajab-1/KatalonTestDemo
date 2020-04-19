d3f90fd6f02eb31664e7601d7d7ebb85e7418ee4
Rshawwa <Rshawwa@L-Rshawwa.eSenseSoftware.com>
2020-04-19 15:51:13 +0300

    Latest Version

---
+++ Test Suites/TestSuiteListner/Listner Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Listner Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fdffcacb-5c86-4191-8801-e30f6767e8a3</testSuiteGuid>
+   <testCaseLink>
+      <guid>5941dc84-e082-4bed-9d80-bf106243e77d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Listner Usage/Case 1 for Listner</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TestSuiteforSetupandTeardown.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteforSetupandTeardown</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8a1623fa-7b84-4541-aefb-d9c24a803b1f</testSuiteGuid>
+   <testCaseLink>
+      <guid>f222c4b5-1f1e-44a0-b8de-572a0184e855</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Listner Usage/Case 1 for Listner</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
